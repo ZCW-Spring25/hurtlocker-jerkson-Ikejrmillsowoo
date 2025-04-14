4895833ec8799b892a651f3dac090a014cc56779
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-14 17:15:56 -0400

    check in

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -21,14 +21,14 @@ public class ItemParser {
 //        if (!valueToParse.contains("[:@^%*]") || !valueToParse.contains(";")|| !matcher.find()) {
 //            throw new ItemParseException();
 //        }
-        list.add(parseSingleItem(value));
+list.add(parseSingleItem(value));
     }
-        for (String value : splitValues) {
-//            if (!valueToParse.contains("[:@^%*]") || !valueToParse.contains(";")) {
-//                throw new ItemParseException();
-//            }
-            list.add(parseSingleItem(value));
-        }
+//        for (String value : splitValues) {
+////            if (!valueToParse.contains("[:@^%*]") || !valueToParse.contains(";")) {
+////                throw new ItemParseException();
+////            }
+//            list.add(parseSingleItem(value));
+//        }
         //loop through the given string
         //separate it into objects and key value pairs
         //any key missing a value should throw and excpetion.
@@ -51,23 +51,15 @@ public class ItemParser {
         String stripped = (matchObjects1.replaceAll(" ").replace("##", " "));
         String[] pairs = stripped.split(" ");
 
-        try {
             for (String pair : pairs) {
                 String[] keyValue = pair.split("[:@^%*]", 2);
                 if (keyValue.length != 2 || keyValue[0] == null || keyValue[1] == null || keyValue[0].trim().isEmpty() || keyValue[1].trim().isEmpty()) {
                     throw new ItemParseException();
                 }
-
-                map.put(keyValue[0].trim().toLowerCase(), keyValue[1].trim().toLowerCase());
+                String key = keyValue[0].trim().toLowerCase();
+                String value = keyValue[1].trim().toLowerCase();
+                map.put(key, value);
             }
-        } catch (ItemParseException e) {
-           throw new ItemParseException();
-        }
-
-
-        if (map.get("name") == null || map.get("price") == null || map.get("type") == null || map.get("expiration") == null) {
-            throw new ItemParseException();
-        }
 
 //        for (String key : map.keySet()) {
 //            System.out.println(key + "=" + map.get(key));
