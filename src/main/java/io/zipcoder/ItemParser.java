e03c86b707ebef0b5ea03056f21516a6b8a29a91
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-14 16:10:39 -0400

    still not passing tests

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -12,11 +12,23 @@ public class ItemParser {
 
     public List<Item> parseItemList(String valueToParse) throws ItemParseException {
         List<Item> list = new ArrayList<>();
+//        Pattern separateKeyAndValue = Pattern.compile("[:@^%*]", Pattern.CASE_INSENSITIVE);
+//        Matcher matcher = separateKeyAndValue.matcher(valueToParse);
+
         String[] splitValues = valueToParse.split("##");
 
-            for (String value : splitValues) {
-                    list.add(parseSingleItem(value));
-            }
+    for (String value : splitValues) {
+//        if (!valueToParse.contains("[:@^%*]") || !valueToParse.contains(";")|| !matcher.find()) {
+//            throw new ItemParseException();
+//        }
+        list.add(parseSingleItem(value));
+    }
+        for (String value : splitValues) {
+//            if (!valueToParse.contains("[:@^%*]") || !valueToParse.contains(";")) {
+//                throw new ItemParseException();
+//            }
+            list.add(parseSingleItem(value));
+        }
         //loop through the given string
         //separate it into objects and key value pairs
         //any key missing a value should throw and excpetion.
@@ -28,30 +40,32 @@ public class ItemParser {
     public Item parseSingleItem(String singleItem) throws ItemParseException {
 
         Map<String, String> map = new HashMap<>();
-        Pattern separateKeyAndValue = Pattern.compile("[:@^%]", Pattern.CASE_INSENSITIVE);
+        Pattern separateKeyAndValue = Pattern.compile("[:@^%*]", Pattern.CASE_INSENSITIVE);
         Pattern separateKeyValue = Pattern.compile(";");
         Pattern separateObjects = Pattern.compile("##");
         Matcher matchObjects = separateKeyAndValue.matcher(singleItem);
         Matcher matchObjects1 = separateKeyValue.matcher(singleItem);
         Matcher matchObjects2 = separateObjects.matcher(singleItem);
 
-        try{
-//            if (!singleItem.contains(";")){
-//                throw new ItemParseException();
-//            } else {
-                String stripped = (matchObjects1.replaceAll(" ").replace("##", " "));
-                String[] pairs = stripped.split(" ");
-                //System.out.println(Arrays.toString(pairs));
-                for (String pair : pairs) {
-                    String[] keyValue = pair.split("[:@^%]", 2);
-                    if (keyValue.length != 2) {
-                        throw new ItemParseException();
-                    }
 
-                    map.put(keyValue[0].toLowerCase(), keyValue[1].toLowerCase());
+        String stripped = (matchObjects1.replaceAll(" ").replace("##", " "));
+        String[] pairs = stripped.split(" ");
+
+        try {
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("[:@^%*]", 2);
+                if (keyValue.length != 2 || keyValue[0] == null || keyValue[1] == null || keyValue[0].trim().isEmpty() || keyValue[1].trim().isEmpty()) {
+                    throw new ItemParseException();
                 }
 
-        } catch (ItemParseException e){
+                map.put(keyValue[0].trim().toLowerCase(), keyValue[1].trim().toLowerCase());
+            }
+        } catch (ItemParseException e) {
+           throw new ItemParseException();
+        }
+
+
+        if (map.get("name") == null || map.get("price") == null || map.get("type") == null || map.get("expiration") == null) {
             throw new ItemParseException();
         }
 
@@ -61,11 +75,6 @@ public class ItemParser {
 
         Item item = new Item(map.get("name"), Double.parseDouble(map.get("price")), map.get("type"), map.get("expiration"));
 
-        //Pattern pattern = Pattern
-        //loop through the given string
-        //separate it into objects and key value pairs
-        //any key missing a value should throw and excpetion.
-        //return single item parsed.
         return item;
     }
 }
